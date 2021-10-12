e0e0665ccac16c99086a8adbe4f23da675723e7c
Harapecopecopeco <yusuke.watanabe@hopin.co.jp>
2021-10-13 02:32:05 +0900

    :+1:Add npm axios

---
+++ array.ts
@@ -0,0 +1,14 @@
+export {}
+let numbers: number[] = [1, 2, 3, 4]
+console.log({numbers})
+
+let matrix: number[][] = [
+    [50, 100],
+    [150, 200]
+]
+console.log({matrix})
+let complex: (number | string | boolean)[] = [1, "abc", true]
+
+console.log({complex})
+
+
