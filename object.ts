e84a44f0b451a99fccccb2312c7246140d9a1e7b
Harapecopecopeco <yusuke.watanabe@hopin.co.jp>
2021-10-16 02:11:00 +0900

    :+1:Add object

---
+++ object.ts
@@ -0,0 +1,11 @@
+export {}
+let profile1: object = {name: "Mam"}
+profile1 = {binary: 1976}
+
+interface Profile {
+    name: string
+}
+
+let profile2: Profile = {name: "Dad"}
+
+profile2 = {name: "aaa"}
